db9faf51baa4d5d4a098f8341d8eb28d04f5c852
Saaketh <narayan.saaketh@gmail.com>
2024-05-19 21:42:43 -0700

    typo

--- chapter_4/device_props.cu
+++ chapter_4/device_props.cu
@@ -16,7 +16,7 @@ int main()
         std::cout << "Max threads per block: " << deviceProp.maxThreadsPerBlock << std::endl;
         std::cout << "Number of SMs: " << deviceProp.multiProcessorCount << std::endl;
         std::cout << "Clock rate: " << deviceProp.clockRate << std::endl;
-        std::cout << "Max threads by dimension (x, y, z): " << deviceProop.maxThreadsDim[0] << ", " << deviceProp.maxThreadsDim[1] << ", " << deviceProp.maxThreadsDim[2] << std::endl;
+        std::cout << "Max threads by dimension (x, y, z): " << deviceProp.maxThreadsDim[0] << ", " << deviceProp.maxThreadsDim[1] << ", " << deviceProp.maxThreadsDim[2] << std::endl;
         std::cout << "Max registers per block (should be same as for SM): " << deviceProp.regsPerBlock << std::endl;
         std::cout << "Number of threads per warp: " << deviceProp.warpSize << std::endl;
         std::cout << std::endl;
