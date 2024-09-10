b5e0b58223121c2f367f2ab0040a660590cf0c18
Saaketh <narayan.saaketh@gmail.com>
2024-09-10 12:54:51 -0400

    kerne

---
+++ chapter_3/matvec.cu
@@ -0,0 +1,82 @@
+#include <iostream>
+
+// kernel function
+__global__
+void matVecKernel(float* Vout, float* Vin, float* Mat, int len){
+    // get col and row indices of the matvec. Each thread computes one element of the output.
+    // this is just a 1D grid....
+    // we are also assuming that Mat is a square matrix, meaning that Vout and Vin are the same length.
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // Bounds checking for the length of the vector.
+    if (idx < len){
+        // For output element at idx, we want to dot product the idx-th row of Mat by Vin
+        float accum = 0.0f;
+        for (int i = 0; i < len; i++){
+            accum += Mat[idx*len + i] * Vin[i];
+        }
+        Vout[idx] = accum;
+    }
+}
+
+// stub function
+void matVec(float* Vout_h, float* Vin_h, float* Mat_h, int len){
+
+    // Initialize and allocate pointers to device memory.
+    float *Vout_d, *Vin_d, *Mat_d;
+
+    // Allocate memory for Vin_d, Vout_d, Mat_d on device
+    cudaMalloc((void**) &Vin_d, len);
+    cudaMalloc((void**) &Vout_d, len);
+    cudaMalloc((void**) &Mat_d, len);
+
+    // Copies from host memory to device memory
+    cudaMemcpy(Vin_d, Vin_h, len, cudaMemcpyHostToDevice);
+    cudaMemcpy(Vout_d, Vout_h, len, cudaMemcpyHostToDevice);
+    cudaMemcpy(Mat_d, Mat_h, len, cudaMemcpyHostToDevice);
+
+    // Launch the kernel
+    // Use a 1D grid of blocks, each with 256 threads.
+    matVecKernel<<<ceil(len/256.0), 256>>>(Vout_d, Vin_d, Mat_d, len);
+
+    // Copy result from device memory to host memory
+    cudaMemcpy(Vout_h, Vout_d, len, cudaMemcpyDeviceToHost);
+
+    // Don't forget to free device memory!!
+    cudaFree(Vin_d);
+    cudaFree(Vout_d);
+    cudaFree(Mat_d);
+}
+
+int main(){
+    std::cout << "MatVec" << std::endl;
+
+    int len = 125;
+
+    unsigned char* Vout_h = new float[len];
+    unsigned char* Vin_h = new float[len];
+    unsigned char* Mat_h = new float[len*len];
+    
+    // Mat_h is all 1s
+    std:fill(Mat_h, Mat_h + len*len, 1.0f);
+
+    // Vin_d will be increasing values
+    for (int i = 0; i < len; i++){
+        Vin_h[i] = (float) i;
+    }
+
+    // call stub which takes care of everything device-side
+    matVec(Vout_h, Vin_h, Mat_h, len);
+
+    std::cout << "Input Vector:";
+    for (int i = 0; i < len; i++) {
+        std::cout << (int) Vin_h[i] << " ";        
+    }
+    std::cout << std::endl;
+
+    std::cout << "Output Vector:";
+    for (int i = 0; i < len; i++) {
+        std::cout << (int) Vout_h[i] << " ";        
+    }
+    std::cout << std::endl;
+}
